62b7f8c7579fb1496c854aedee2dd069661b0569
Maks <kmv80@yandex.ru>
2022-09-12 22:41:53 +0200

    added posgresql to docker

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator function
+"""
+
+
+def add(x, y):
+    """Add x and y and return result"""
+
+    return x + y
+
+def sub(x, y):
+    """Substract x from y and return result"""
+
+    return x - y
